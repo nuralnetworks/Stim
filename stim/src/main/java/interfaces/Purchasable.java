6910b2e7eeebefde69bdb10c0a77abdc2e8a7706
Nural <nural.shukurlu.std@bhos.edu.az>
2026-04-20 21:50:44 +0400

    Delete stim directory

--- stim/src/main/java/interfaces/Purchasable.java
+++
@@ -1,8 +0,0 @@
-package interfaces;
-
-import models.Player;
-
-public interface Purchasable {
-    boolean purchase(Player player);
-    boolean refund(Player player);
-}
--- stim/src/main/java/patterns/RecommendationStrategy.java
+++
@@ -1,9 +0,0 @@
-package patterns;
-
-import models.Product;
-import models.Player;
-import java.util.List;
-
-public interface RecommendationStrategy {
-    List<Product> recommendGames(Player player, List<Product> allProducts);
-}
